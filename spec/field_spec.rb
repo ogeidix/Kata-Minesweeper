8281e8d41ed0d876c13b4c1043bc13a74516b2e8
ogeidix <diegogiorgini@gmail.com>
2011-06-16 11:42:18 +0200

    Added a list of specs

---
+++ spec/field_spec.rb
@@ -0,0 +1,17 @@
+
+describe "A field instance" do
+  describe "of 1x1" do
+    pending "should take no-mine input and reveal its content"
+    pending "should take one-mine input and reveal its content"
+  end
+  
+  describe "of 1x4" do  
+    pending "should take no-mine input and reveal its content"
+    pending "should take one-mine input and reveal its content"
+  end
+  
+  describe "of 4x4" do  
+    pending "should take no-mine input and reveal its content"
+    pending "should take one-mine input and reveal its content"
+  end
+end
